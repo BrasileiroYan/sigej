660605dd8077991a432d86243a67f4b6b6614c92
Yan Pedro <yanpedrobrasileiro@gmail.com>
2025-11-19 12:11:20 -0300

    feat: adicionado arquivos inciais do projeto

---
+++ src/main/java/com/example/SIGEJ/SigejApplication.java
@@ -0,0 +1,13 @@
+package com.example.SIGEJ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigejApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigejApplication.class, args);
+	}
+
+}
